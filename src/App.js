b88a6df147b0ecb129de3aaca9d5b332a76e6b14
Cheuky Li <lich25@oocl.com>
2025-09-15 11:06:05 +0800

    feat: add TodoDetailPage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
-import {createBrowserRouter, NavLink, Outlet, RouterProvider} from "react-router";
+import {createBrowserRouter, NavLink, Outlet, RouterProvider, useParams} from "react-router";
 import {ErrorPage} from "./pages/ErrorPage";
 import {HomePage} from "./pages/HomePage";
+import {useContext, useReducer} from "react";
+import {TodoContext} from "./contexts/TodoContext";
+import {TodoItem} from "./components/TodoItem";
+import {todoReducer} from "./reducers/TodoReducer";
 
 export const initState = [];
 
@@ -11,12 +15,27 @@ function DefaultLayout() {
             <nav>
                 <ul>
                     <li><NavLink to={"/"}>Home</NavLink></li>
+                    <li><NavLink to={"/todos/1"}>ID 1</NavLink></li>
                 </ul>
             </nav>
         </header>
         <main>
             <Outlet/>
         </main>
+    </div>
+}
+
+function TodoDetailPage(){
+    const {id} = useParams()
+    const {state, dispatch} = useContext(TodoContext)
+    const todo = state.filter((todo) => todo.id === parseInt(id))
+
+    if (todo.length === 0){
+        return <div>Not found Todo</div>
+    }
+
+    return <div>
+        <TodoItem todo = {todo[0]} index = {id}/>
     </div>
 }
 
@@ -29,16 +48,23 @@ const routes = createBrowserRouter([
             {
                 path: "/",
                 element: <HomePage/>
+            },
+            {
+                path: "/todos/:id",
+                element: <TodoDetailPage/>
             }
         ]
     }
 ]);
 
 function App() {
+    const [state, dispatch] = useReducer(todoReducer, initState);
 
     return (
         <div>
+            <TodoContext.Provider value={{state, dispatch}}>
             <RouterProvider router={routes}/>
+            </TodoContext.Provider>
         </div>
     );
 }
