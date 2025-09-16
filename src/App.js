737ee98984350ad58c2fe324cbd53e3d5d9d489a
Cheuky Li <lich25@oocl.com>
2025-09-16 10:24:51 +0800

    feat(interceptor): add 404 axios render

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ import {TodoContext} from "./contexts/TodoContext";
 import {todoReducer} from "./reducers/TodoReducer";
 import {routes} from "./routes/Routes";
 import {useTodoService} from "./useTodoService";
+import '@ant-design/v5-patch-for-react-19';
+import { unstableSetRender } from 'antd';
+import { createRoot } from 'react-dom/client';
+
+unstableSetRender((node, container) => {
+    container._reactRoot ||= createRoot(container);
+    const root = container._reactRoot;
+    root.render(node);
+    return async () => {
+        await new Promise((resolve) => setTimeout(resolve, 0));
+        root.unmount();
+    };
+});
 
 function App() {
     const [state, dispatch] = useReducer(todoReducer, []);
--- src/api/mockApi.js
+++ src/api/mockApi.js
@@ -1,7 +1,23 @@
 import axios from "axios";
+import {message} from "antd";
 
-export const api = axios.create({
+const api = axios.create({
     baseURL: "https://68c7ac8e5d8d9f51473287a1.mockapi.io/",
     headers: {"Content-Type": "application/json"},
     timeout: 10_000
 })
+
+api.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        const {status, data} = error.response;
+        if (status === 404){
+            message.error(error.message, 10_000).then(r => {});
+        }
+        return Promise.reject(error);
+    }
+);
+
+export {api}
